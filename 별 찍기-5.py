033023ce54a08e496628165a6adec92d2b8a1892
sangmin906 <psm31525@a.ut.ac.kr>
2025-03-27 00:06:11 +0900

    별 찍기-5(수정).py

--- 별 찍기-5.py
+++ 별 찍기-5.py
@@ -2,7 +2,10 @@ n=int(input())
 for i in range(n):
     for j in range(1,n*2):
         if j>=n-i and j<=n+i:
+    for j in range(n*2-1):
+        if n-i-1<=j<=n+i-1:
             print("*",end="")
         else:
+        elif j<=n-i-1:
             print(" ",end="")
     print()
