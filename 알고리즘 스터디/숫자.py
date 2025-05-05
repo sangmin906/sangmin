306d82d2941e3eaf2073737d21d85a2e5c548b79
sangmin906 <psm31525@a.ut.ac.kr>
2025-05-05 19:52:26 +0900

    숫자(수정).py

--- 알고리즘 스터디/숫자.py
+++ 알고리즘 스터디/숫자.py
@@ -3,8 +3,21 @@ if a<b:
     a,b=b,a
 if a-b==0:
     print(a-b)
+a=list(map(int,input().split()))
+if a[0]<a[1]:
+    big=a[1]
+    small=a[0]
+elif a[0]>a[1]:
+    big=a[0]
+    small=a[1]
 else:
     print(a-b-1)
     if a-b!=1:
         for i in range(b+1,a):
+    print(a[0]-a[1])
+    exit()
+if big-small>=1:
+    print(big-small-1)
+    if big-small!=1:
+        for i in range(small+1,big):
             print(i,end=" ")
